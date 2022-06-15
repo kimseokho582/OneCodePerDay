dfc8db3a2be0d3c5d604ed8a29bc9333675b2d34
kimseokho582 <gjrehf@naver.com>
2022-06-15 12:56:04 +0900

    220615  > 최소힙 1927

---
+++ 1927.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int tmp;
+        cin >> tmp;
+        if (tmp == 0)
+        {
+            if (pq.empty())
+            {
+                cout << 0 << "\n";
+            }
+            else
+            {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }
+        else
+        {
+            pq.push(tmp);
+        }
+    }
+}
