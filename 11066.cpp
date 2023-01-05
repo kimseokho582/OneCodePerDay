a5286f11c4cc4410585dedcedb80331745f6c83b
kimseokho582 <gjrehf@naver.com>
2023-01-05 16:35:31 +0900

    230105

--- 11066.cpp
+++ 11066.cpp
@@ -1,41 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
-//틀린 풀이
+int table[501];
+int sum[501] = {
+    0,
+};
+int dp[501][501];
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> table[i];
+            sum[i] = sum[i - 1] + table[i];
+        }
 
-// #include <iostream>
-// #include <queue>
-
-// using namespace std;
-
-// int main()
-// {
-//     int t;
-//     cin >> t;
-//     while (t--)
-//     {
-//         int n, ans = 0;
-//         cin >> n;
-//         priority_queue<int, vector<int>, greater<int>> pq;
-//         for (int i = 0; i < n; i++)
-//         {
-//             int num;
-//             cin >> num;
-//             pq.push(num);
-//         }
-
-//         while (1)
-//         {
-//             int a = pq.top();
-//             cout << "a: " << a << endl;
-//             pq.pop();
-//             int b = pq.top();
-//             pq.pop();
-//             cout << "b: " << b << endl;
-//             ans += (a + b);
-//             cout << "ans: " << ans << endl;
-//             if (pq.empty())
-//                 break;
-//             pq.push(a + b);
-//         }
-//         cout << ans << "\n";
-//     }
-// }
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                dp[j][i + j] = 1000000000;
+                for (int k = j; k < i + j; k++)
+                {
+                    dp[j][i + j] = min(dp[j][i + j], dp[j][k] + dp[k + 1][i + j] + sum[i + j] - sum[j - 1]);
+                }
+            }
+        }
+        cout << dp[1][n] << endl;
+    }
+}
